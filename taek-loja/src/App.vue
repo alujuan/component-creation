4870630a09dc1266d045ade2cf853ec17f85e75a
alujuan <99190123+alujuan@users.noreply.github.com>
2022-11-15 15:56:19 -0300

    Add files via upload

---
+++ taek-loja/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import MenuLateral from "@/components/MenuLateral.vue";
+import BlocoPrincipal from "@/components/BlocoPrincipal.vue";
+import BlocoCabecalho from "@/components/BlocoCabecalho.vue";
+import BlocoRodape from "@/components/BlocoRodape.vue";
+export default {
+  components: { MenuLateral, BlocoPrincipal, BlocoRodape, BlocoCabecalho },
+};
+</script>
+<template>
+  <BlocoCabecalho />
+  <MenuLateral />
+  <BlocoPrincipal />
+  <BlocoRodape />
+</template>
+<style></style>
---
+++ taek-loja/src/components/NovoBloco.vue
@@ -0,0 +1,9 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <article class="block">Desculpa pelo atraso!</article>
+</template>
+
+<style></style>
